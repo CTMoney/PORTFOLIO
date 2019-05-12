ba0d656b2defacb94c67b4e1a309c49bda1bf0db
Cat <money1062@live.com>
2019-05-12 15:24:16 -0400

    update to routing

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import NavBar from "./components/navbar"
 import Contact from "./components/contact"
 import Home from "./components/home"
 import About from "./components/about"
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 import "./index.css"
 import "bootstrap"
 
 const App = () => {
   return (
     <Router>
-      <>
+      <Switch>
         <NavBar />
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
-      </>
+      </Switch>
     </Router>
   )
 }
